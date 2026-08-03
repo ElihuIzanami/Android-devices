ba56e6d04161e4bccc0f10ad5f70722ce8184317
Izanami49 <elihu.montoya.estrada@gmail.com>
2026-08-02 18:56:27 -0600

    Proyecto Android Devices

---
+++ app/src/main/java/com/eime/devices/Specs.kt
@@ -0,0 +1,12 @@
+package com.eime.devices
+
+
+/*
+"data": {
+      "color": "Cloudy White",
+      "capacity": "128 GB"
+    }
+ */
+data class Specs(
+    val color: String?,
+    val capacity: String?)
